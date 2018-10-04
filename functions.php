d97f7f92625f831f0e6cfd0e5a68652c09f06e74
Ben Moore <bmo@skypress.io>
2018-10-04 00:00:28 -0500

    Big re-arrange, added more templates and bootstrap

--- functions.php
+++ functions.php
@@ -42,11 +42,39 @@ skyforgeRequireComposerAutoloader();
  */
 function skyforgeEnqueueStylesheets()
 {
-    // Parent
-    wp_enqueue_style('skyforge-style', get_template_directory_uri() . '/style.css');
-    // Child
+    /*
+        Enqueue CSS Files
+     */
+    wp_register_style('skyforge_bootstrap_css', '//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css', [], '4.1.3');
+    wp_register_style('skyforge_style', get_template_directory_uri() . '/style.css', [], '0.1.0'); // Parent CSS
     if (file_exists(get_stylesheet_directory() . '/style.css')) {
-        wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css');
+        wp_register_style('skyforge_child_style', get_stylesheet_directory_uri() . '/style.css'); // Child CSS
     }
+    wp_enqueue_style('skyforge_bootstrap_css', 'skyforge_bootstrap_css');
+    wp_enqueue_style('skyforge_style', 'skyforge_style');
+    wp_enqueue_style('skyforge_child_style', 'skyforge_child_style');
+
+    /*
+        Enqueue Javascript Files
+     */
+    wp_register_script('skyforge_bootstrap_js', '//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', ['jquery'], '4.1.3', true);
+    wp_enqueue_script('skyforge_bootstrap_js', 'skyforge_bootstrap_js');
 }
 add_action('wp_enqueue_scripts', 'skyforgeEnqueueStylesheets');
+
+/**
+ * Register Theme Navigation Menus
+ *
+ * @method registerThemeMenu
+ *
+ * @since 0.1.0
+ *
+ */
+function registerThemeMenu()
+{
+    $args = apply_filters('skyforge_theme_menus', [
+        'main'  => 'Main Navigation Menu'
+    ]);
+    register_nav_menus($args);
+}
+registerThemeMenu();
--- src/Init.php
+++ src/Init.php
@@ -9,6 +9,9 @@ namespace SkyForge;
  * @var \SkyForge\Mustache $mustache
  * @var int $post_id
  * @var string $template_type
+ * @var string $data_filter
+ * @var string nav_slug_filter
+ * @var string nav_slug_default
  */
 class Init
 {
@@ -35,7 +38,35 @@ class Init
      *
      * @var string
      */
-    public $template_type;
+    public $template_type = 'page';
+
+    /**
+     * SkyForge Render Data filter slug
+     *
+     * @var string
+     */
+    public $data_filter = 'skyforge_render_data';
+
+    /**
+     * Navigation Slug filter slug
+     *
+     * @var string
+     */
+    public $nav_slug_filter = 'skyforge_nav_slug';
+
+    /**
+     * Missing Filter slug
+     *
+     * @var string
+     */
+    public $missing_filter_slug = 'skyforge_not_found_slug';
+
+    /**
+     * Default navigation slug
+     *
+     * @var string
+     */
+    public $nav_slug_default = 'main';
 
     /**
      * Init constructor
@@ -61,31 +92,79 @@ class Init
      */
     public function render() : string
     {
-        $data = $this->getPostData();
-        if (empty($this->template_type)) {
-            $this->template_type = 'page'; // Default to page
-        }
-        $html = $this->mustache->loadTemplate($this->template_type)->render($data);
-        return $html;
+        $data   = $this->getRenderData();
+        $render = [
+            'header'                => $this->getHeaderData(),
+            $this->template_type    => $this->getRenderData()
+        ];
+        $filtered_data = apply_filters($this->data_filter, $render);
+        // print_r($filtered_data['nav']);
+        return $this->mustache->loadTemplate($this->template_type)->render($filtered_data);
+    }
+
+    public function getHeaderData()
+    {
+        $data = [
+            'title' => get_bloginfo('name'),
+            'nav'   => $this->getNavData()
+        ];
+        return $data;
+    }
+
+    /**
+     * Get Navigation Data
+     *
+     * @method getNavData
+     *
+     * @since 0.1.0
+     *
+     * @return mixed object | bool
+     */
+    public function getNavData()
+    {
+        $nav_slug = apply_filters($this->nav_slug_filter, $this->nav_slug_default);
+        return wp_get_nav_menu_items($nav_slug);
     }
 
     /**
      * Get Data from Rest Request based on slug
      *
-     * @method getPostData
+     * @method getRenderData
      *
      * @since 0.1.0
      *
      * @return object $data
      */
-    public function getPostData() : object
+    public function getRenderData() : object
     {
         $post = get_post();
+        if (! $post->ID || null === $post->ID) {
+            return $this->getMissingData();
+        }
         $this->template_type    = strtolower($post->post_type);
         $api_endpoint           = $this->determineApiEndpoint($post->ID, $post->post_type);
         $request                = $this->getNewRestRequest($api_endpoint);
         $data                   = $this->getDataFromRestServer($request);
         return $data;
+    }
+
+    /**
+     * Get Data for Missing Post request
+     *
+     * @method getMissingData
+     *
+     * @since 0.1.0
+     *
+     * @return object
+     */
+    public function getMissingData()
+    {
+        $this->template_type = '404';
+        $data = apply_filters($this->missing_filter_slug, (object)[
+            'title' => '404 Not Found',
+            'text'  => 'The page requested could not be found'
+        ]);
+        return (object)$data;
     }
 
     /**
